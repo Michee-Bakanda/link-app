b3d40ce589ec49e68fdff3bd6664845ad22ed735
michee <micbakanda@gmail.com>
2022-07-06 02:11:06 +0200

    link preview created

--- screens/Post.js
+++ screens/Post.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import { useState } from 'react'
-import {SafeAreaView, ScrollView, Text, TextInput,View} from "react-native"
+import { SafeAreaView, ScrollView, Text, TextInput, View } from "react-native"
 // import {Text, View, } from 'react-native'
 import FooterTabs from '../components/nav/FooterTabs'
 import SubmitButton from '../components/auth/SubmitButton'
 // import { LinkContext } from "../context/link";
-// import ogs from "@uehreka/open-graph-scraper-react-native";
+import ogs from "@uehreka/open-graph-scraper-react-native";
 // import urlRegex from "url-regex";
+import urlRegex from "url-regex";
+// const ogs = require('open-graph-scraper');
+// import ogs from 'open-graph-scraper';
 // import PreviewCard from "../components/links/PreviewCard";
 import axios from "axios";
+import PreviewCard from "../components/Links/PreviewCard"
 // import urlRegex from "url-regex";
 // import ogs from "@uehreka/open-graph-scraper-react-native";
 
@@ -16,15 +20,15 @@ import axios from "axios";
 
 const Post = () => {
 
-   // context
+  // context
   //  const [links, setLinks] = useContext(LinkContext);
   // state
-   const [link, setLink]= useState('')
-   const [title, setTitle] = useState('')
-   const [loading, setLoading] = useState('')
-   const [urlPreview, setUrlPreview] = useState({});
+  const [link, setLink] = useState('')
+  const [title, setTitle] = useState('')
+  const [loading, setLoading] = useState('')
+  const [urlPreview, setUrlPreview] = useState({});
 
-   const handleChange=(text)=>{
+  const handleChange = (text) => {
     //  
     try {
       setLoading(true);
@@ -35,19 +39,23 @@ const Post = () => {
           // console.log(results);
           if (results.success) {
             setUrlPreview(results);
+            alert("success")
           }
           setLoading(false);
+         
         });
       } else {
         setLoading(false);
+     
       }
     } catch (err) {
       console.log(err);
       setLoading(false);
+      alert("fail")
     }
-   }
+  }
 
-   const handleSubmit = async () => {
+  const handleSubmit = async () => {
     // console.log("title and link => ", title, link, urlPreview);
     if (!link || !title) {
       alert("Paste url and give it a nice title 😎");
@@ -61,33 +69,44 @@ const Post = () => {
       });
       // console.log("data => ", data);
       // update link context
-      setLinks([data, ...links]);
+      setLink([data, ...link]);
       setTimeout(() => {
         alert("🎊 Link posted");
         navigation.navigate("Home");
       }, 500);
     } catch (err) {
       console.log(err);
+      // alert(err)
     }
   };
+
 
   return (
     // <SafeAreaView style={{flex: 1, justifyContent: "space-between"}}>
     //     <Text>Posts</Text>
     //     <FooterTabs />
     // </SafeAreaView>
-    <SafeAreaView style={{flex: 1}}>
-         <ScrollView>
-               <Text style={{textAlign:"center"}}>Paste website url</Text>
-               <TextInput placeholder='Paste the url' style={{borderWidth: 1,borderColor: "gray",height: 50, marginVertical:30,marginHorizontal:10,borderRadius: 30, padding: 15}} value={link} onChangeText={text => setLink(text)} />
-               <TextInput placeholder='Give it a title' style={{borderWidth: 1,borderColor: "gray",height: 50, marginVertical:30,marginHorizontal:10,borderRadius: 30, padding: 15}} value={title} onChangeText={text => setTitle(text)} />
+    <SafeAreaView style={{ flex: 1 }}>
+      <ScrollView>
+        <Text style={{ textAlign: "center" }}>Paste website url</Text>
+        <TextInput onChangeText={(text) => handleChange(text)} placeholder='Paste the url' style={{ borderWidth: 1, borderColor: "gray", height: 50, marginVertical: 30, marginHorizontal: 10, borderRadius: 30, padding: 15 }} value={link}  />
+        <TextInput placeholder='Give it a title' style={{ borderWidth: 1, borderColor: "gray", height: 50, marginVertical: 30, marginHorizontal: 10, borderRadius: 30, padding: 15 }} value={title} onChangeText={text => setTitle(text)} />
 
-               <View style={{paddingTop:25}}>
-                   <SubmitButton title="Submit" handleSubmit={handleSubmit} loading={loading} />
-               </View>
-               <Text>{JSON.stringify(urlPreview, null, 4)}</Text>
-         </ScrollView>
-         <FooterTabs />
+        {urlPreview.success && (
+           <View style={{
+             marginTop: 30,
+             alignItems: 'center',
+           }}>
+              <PreviewCard {...urlPreview} />
+           </View>
+        )}
+
+        <View style={{ paddingTop: 25 }}>
+          <SubmitButton title="Submit" handleSubmit={handleSubmit} loading={loading} />
+        </View>
+        <Text>{JSON.stringify(urlPreview, null, 4)}</Text>
+      </ScrollView>
+      <FooterTabs />
     </SafeAreaView>
   )
 }
